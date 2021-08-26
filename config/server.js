e0c5b3100a17fc4913be2b7a9249ff59199ddb13
Cindy Le <cindyledev@gmail.com>
2021-08-26 12:41:27 -0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fed147ea464accccc150e361d7bd018c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '364d6618-140b-40b8-a8a9-9e98f14ac2bb'
+};
