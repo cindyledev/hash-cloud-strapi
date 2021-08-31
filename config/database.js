4c838af94a8c830c86df2ed718a62d3884bdfaae
Cindy Le <cindyledev@gmail.com>
2021-08-30 23:54:59 -0400

    adding event content type and 6 events

--- config/database.js
+++ config/database.js
@@ -4,14 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        // client: 'sqlite',
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT, 5432'),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        // client: 'postgres',
+        // host: env('DATABASE_HOST', '127.0.0.1'),
+        // port: env.int('DATABASE_PORT, 5432'),
+        // database: env('DATABASE_NAME', 'strapi'),
+        // username: env('DATABASE_USERNAME', ''),
+        // password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         useNullAsDefault: true,
